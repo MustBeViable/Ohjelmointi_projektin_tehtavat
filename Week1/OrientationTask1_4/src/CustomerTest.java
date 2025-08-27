d65b0d17f2d32652a0d9edfac99b3766e777a2a1
Elias <elias1rinne@gmail.com>
2025-08-27 10:05:04 +0300

    Task 4 completed

---
+++ Week1/OrientationTask1_4/src/CustomerTest.java
@@ -0,0 +1,27 @@
+import java.util.LinkedList;
+import java.util.ArrayList;
+
+public class CustomerTest {
+
+    public static void main(String[] args) {
+        LinkedList<Customer> customerList = new LinkedList<>();
+        ArrayList<Customer> dequedCustomers = new ArrayList<>();
+        int testCustomerCount = 100;
+        for (int i=0; i<testCustomerCount; i++) {
+            Customer customer = new Customer();
+            customer.setId(i+1);
+            customer.setStartTime(System.nanoTime());
+            customerList.addFirst(customer);
+        }
+        while (!customerList.isEmpty()) {
+            Customer customer = customerList.removeLast();
+            customer.setEndTime(System.nanoTime());
+            dequedCustomers.add(customer);
+        }
+
+        for (Customer customer : dequedCustomers) {
+            System.out.printf("%d. Customer came at %d and left %d. Waiting time was %d\n",
+                    customer.getId(), customer.getStartTime(), customer.getEndTime(), customer.spentTime());
+        }
+    }
+}
